a70e45c69bdd348476a039e6509731170c259a50
unknown <bkbagchi.dipto@gmail.com>
2025-10-05 16:23:42 +0600

    auth middleware added

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized: No token provided" });
+    }
+
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded)
+      return res.status(401).json({ message: "Unauthorized: Invalid token" });
+
+    const { id, email, role } = decoded;
+    req.user = { _id: id, email, role };
+
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized: Invalid token" });
+  }
+};
+
+export default authMiddleware;
