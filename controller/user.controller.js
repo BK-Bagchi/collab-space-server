e2fe9c04a9bbde2a7c8a2668f65a07df39bd0a6d
unknown <bkbagchi.dipto@gmail.com>
2025-10-05 14:51:08 +0600

    controller added to let user read and updated profile

---
+++ controller/user.controller.js
@@ -0,0 +1,25 @@
+import User from "../models/user.model.js";
+
+export const getMe = async (req, res) => {
+  const { _id } = req.body;
+  try {
+    // const user = await User.findById(req.user._id);
+    const user = await User.findById(_id).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message || "Internal server error" });
+  }
+};
+export const putMe = async (req, res) => {
+  const { _id } = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(_id, req.body).select(
+      "-password"
+    );
+    if (!user) return res.status(404).json({ message: "User not updated" });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message || "Internal server error" });
+  }
+};
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,14 @@
 import express from "express";
+import { getMe, putMe } from "../controller/user.controller.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/check", (req, res) => {
   res.send("user route");
 });
+
+userRouter.get("/me", getMe);
+
+userRouter.put("/me", putMe);
 
 export default userRouter;
