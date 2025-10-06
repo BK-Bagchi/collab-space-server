1266c0279b1033e3c699fe5e60ee3ce213b1beb9
unknown <bkbagchi.dipto@gmail.com>
2025-10-06 16:40:24 +0600

    controller added to search a project

--- routes/search.route.js
+++ routes/search.route.js
@@ -1,9 +1,12 @@
 import express from "express";
+import { searchProjects } from "../controller/project.controller.js";
 
 const searchRouter = express.Router();
 
 searchRouter.get("/check", (req, res) => {
   res.send("search route");
 });
+
+searchRouter.get("/projects", searchProjects);
 
 export default searchRouter;
