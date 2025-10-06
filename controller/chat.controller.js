b431b74c517f1e9f0b0d37c8975dd0b0213a00ab
unknown <bkbagchi.dipto@gmail.com>
2025-10-06 16:10:37 +0600

    controller to upload file

--- controller/chat.controller.js
+++ controller/chat.controller.js
@@ -1,6 +1,6 @@
 import Chat from "../models/chat.model.js";
 
-export const getAllChats = async (req, res) => {
+export const getAllChatsOfProject = async (req, res) => {
   const { id } = req.params; // project ID
   try {
     const chats = await Chat.find({ project: id })
@@ -28,6 +28,21 @@ export const sendMessage = async (req, res) => {
     res.status(200).json({ chat, message: "Message sent successfully" });
   } catch (error) {
     console.error("sendMessage error", error);
+    res.status(500).json({ message: error.message || "Internal server error" });
+  }
+};
+
+export const uploadFile = async (req, res) => {
+  const { id } = req.params; // project ID
+  const { sender, attachment, type = "FILE" } = req.body;
+  try {
+    const chatData = { project: id, sender, attachment, type };
+    const chat = await Chat.create(chatData);
+
+    if (!chat) return res.status(404).json({ message: "Chat not created" });
+    res.status(200).json({ chat, message: "File uploaded successfully" });
+  } catch (error) {
+    console.error("uploadFile error", error);
     res.status(500).json({ message: error.message || "Internal server error" });
   }
 };
