1a24abfc137ca90b9d339c205b400a994eb2a67e
unknown <bkbagchi.dipto@gmail.com>
2025-10-06 16:19:41 +0600

    controllers are assigned to routes

--- routes/chat.route.js
+++ routes/chat.route.js
@@ -1,9 +1,20 @@
 import express from "express";
+import {
+  getAllChatsOfProject,
+  sendMessage,
+  uploadFile,
+} from "../controller/chat.controller.js";
 
 const chatRouter = express.Router();
 
 chatRouter.get("/check", (req, res) => {
   res.send("chat route");
 });
+
+chatRouter.get("/:id", getAllChatsOfProject);
+
+chatRouter.post("/:id", sendMessage);
+
+chatRouter.post("/:id/attachment", uploadFile);
 
 export default chatRouter;
