10529cde7072eb63fc4f8c49b1c9dba18a950824
unknown <bkbagchi.dipto@gmail.com>
2025-10-06 15:20:13 +0600

    controller to mark a msg as read

--- controller/notification.controller.js
+++ controller/notification.controller.js
@@ -38,3 +38,23 @@ export const getSingleNotification = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const markAsRead = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const notification = await Notification.findByIdAndUpdate(
+      id,
+      { read: true },
+      { new: true }
+    );
+    if (!notification)
+      return res.status(404).json({ message: "Notification not found" });
+
+    res
+      .status(200)
+      .json({ notification, message: "Notification marked as read" });
+  } catch (error) {
+    console.log("markAsRead error", error);
+    res.status(500).json({ message: error.message });
+  }
+};
