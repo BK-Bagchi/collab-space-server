66f73c6abbb07bce3a426fa558aa5e51818953d6
unknown <bkbagchi.dipto@gmail.com>
2025-10-06 12:51:34 +0600

    Project controllers assigned to routes

--- routes/project.route.js
+++ routes/project.route.js
@@ -1,9 +1,32 @@
 import express from "express";
+import {
+  createProject,
+  deleteProject,
+  getProjectDetails,
+  getProjectMembers,
+  getUserProjects,
+  inviteMember,
+  updateProject,
+} from "../controller/project.controller.js";
 
 const projectRouter = express.Router();
 
 projectRouter.get("/check", (req, res) => {
   res.send("Project route");
 });
+
+projectRouter.post("/", createProject);
+
+projectRouter.get("/user", getUserProjects);
+
+projectRouter.get("/:id", getProjectDetails);
+
+projectRouter.put("/:id", updateProject);
+
+projectRouter.delete("/:id", deleteProject);
+
+projectRouter.post("/:id/invite", inviteMember);
+
+projectRouter.get("/:id/members", getProjectMembers);
 
 export default projectRouter;
