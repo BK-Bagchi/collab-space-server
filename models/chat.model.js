3d4bf40baac570155bb356ecdec5c6c6c4441846
unknown <bkbagchi.dipto@gmail.com>
2025-10-12 11:07:40 +0600

    chat model updated for better and conditional chatting handle

--- models/chat.model.js
+++ models/chat.model.js
@@ -2,18 +2,32 @@ import mongoose from "mongoose";
 
 const ChatSchema = new mongoose.Schema(
   {
-    project: { type: mongoose.Schema.Types.ObjectId, ref: "Project" },
     sender: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
+    receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    project: { type: mongoose.Schema.Types.ObjectId, ref: "Project" },
     content: String,
-    attachment: String,
+    attachment: { type: mongoose.Schema.Types.ObjectId, ref: "File" },
     type: { type: String, enum: ["TEXT", "FILE"], default: "TEXT" },
   },
   { timestamps: true }
 );
+
+ChatSchema.pre("validate", function (next) {
+  if (!this.receiver && !this.project)
+    return next(
+      new Error("Either 'receiver' or 'project' must be provided in chat.")
+    );
+
+  if (this.receiver && this.project)
+    return next(
+      new Error("Both 'receiver' and 'project' cannot be provided in chat.")
+    );
+  next();
+});
 
 const Chat = mongoose.model("Chat", ChatSchema);
 export default Chat;
