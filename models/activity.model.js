194c7148fbb2b9b3de3ba0e3834ff7c1f7a3f191
unknown <bkbagchi.dipto@gmail.com>
2025-11-13 16:45:17 +0600

    new model added to show user activity

---
+++ models/activity.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const ActivitySchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    type: {
+      type: String,
+      enum: [
+        "CREATE_PROJECT",
+        "UPDATE_PROJECT",
+        "DELETE_PROJECT",
+        "ADD_TASK",
+        "UPDATE_TASK",
+        "COMPLETE_TASK",
+        "DELETE_TASK",
+        "ADD_MEMBER",
+        "REMOVE_MEMBER",
+        "SEND_MESSAGE",
+        "UPLOAD_FILE",
+      ],
+      required: true,
+    },
+    message: { type: String, required: true },
+    relatedChat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
+    relatedTask: { type: mongoose.Schema.Types.ObjectId, ref: "Task" },
+    relatedProject: { type: mongoose.Schema.Types.ObjectId, ref: "Project" },
+  },
+  { timestamps: true }
+);
+
+const Activity = mongoose.model("Activity", ActivitySchema);
+export default Activity;
